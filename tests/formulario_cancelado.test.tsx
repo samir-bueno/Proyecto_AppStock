ea48ffce0b37fc624cb6743badd9088cce0414c1
samir-bueno <samirbueno585@gmail.com>
2025-10-12 15:01:35 -0300

    Se hizo un test el cual al cancelar el formulario de agregar cliente fiado no guarde datos

---
+++ tests/formulario_cancelado.test.tsx
@@ -0,0 +1,23 @@
+import FormularioParaAgregarFiado from "@/components/fiados/agregar_cliente_fiado/formulario_para_agregar_fiado";
+import {
+  fireEvent,
+  render,
+} from "@testing-library/react-native";
+
+test("Al cancelar el formulario, se debe cerrar sin guardar datos", () => {
+  let formularioCerrado = false;
+  let datosGuardados = false;
+
+  const { getByText } = render(
+    <FormularioParaAgregarFiado 
+      alCerrarElFormulario={() => { formularioCerrado = true; }} 
+      alGuardarLosDatosDelFormulario={() => { datosGuardados = true; }} 
+      agregandoCliente={false}
+    />
+  );
+
+  fireEvent.press(getByText("Cancelar"));
+
+  expect(formularioCerrado).toBe(true);   
+  expect(datosGuardados).toBe(false); 
+});
